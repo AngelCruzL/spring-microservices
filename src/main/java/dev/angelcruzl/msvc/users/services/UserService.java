64f63fe235d5df4df51055d8101c79a600671bff
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2024-03-28 18:46:17 -0600

    Add user service

---
+++ src/main/java/dev/angelcruzl/msvc/users/services/UserService.java
@@ -0,0 +1,16 @@
+package dev.angelcruzl.msvc.users.services;
+
+import dev.angelcruzl.msvc.users.models.entities.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+    List<User> findAll();
+
+    Optional<User> findById(Long id);
+
+    User save(User user);
+
+    void deleteById(Long id);
+}
