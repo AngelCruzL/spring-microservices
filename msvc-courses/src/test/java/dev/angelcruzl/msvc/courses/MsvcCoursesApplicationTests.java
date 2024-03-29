cafc856352784f67789246474685545f4523b724
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2024-03-28 22:08:34 -0600

    Add courses microservice

---
+++ msvc-courses/src/test/java/dev/angelcruzl/msvc/courses/MsvcCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.angelcruzl.msvc.courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcCoursesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
