bf5988192f747249837f87b72120d39461e708c2
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2024-05-21 13:21:07 -0600

    Add lombok dependency on users microservice

--- msvc-users/src/main/java/dev/angelcruzl/msvc/users/models/entities/User.java
+++ msvc-users/src/main/java/dev/angelcruzl/msvc/users/models/entities/User.java
@@ -4,10 +4,17 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
 
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -23,35 +30,4 @@ public class User {
     @NotBlank
     private String password;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
