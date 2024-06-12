088f05727ffc9c7162c204abc8e0671a6138b93c
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2024-06-11 22:23:36 -0600

    Update auth config at users microservice

--- msvc-users/src/main/java/dev/angelcruzl/msvc/users/config/WebSecurityConfig.java
+++ msvc-users/src/main/java/dev/angelcruzl/msvc/users/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package dev.angelcruzl.msvc.users.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,12 +22,19 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated()
+        http.authorizeRequests()
+            .requestMatchers("/authorized").permitAll()
+            .requestMatchers(HttpMethod.GET, "/", "/{id}").hasAnyAuthority("SCOPE_read", "SCOPE_write")
+            .requestMatchers(HttpMethod.POST, "/").hasAuthority("SCOPE_write")
+            .requestMatchers(HttpMethod.PUT, "/{id}").hasAuthority("SCOPE_write")
+            .requestMatchers(HttpMethod.DELETE, "/{id}").hasAuthority("SCOPE_write")
+            .anyRequest().authenticated()
             .and()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
             .oauth2Login(oauth2Login -> oauth2Login.loginPage("/oauth2/authorization/msvc-users-client"))
-            .oauth2Client(withDefaults());
+            .oauth2Client(withDefaults())
+            .oauth2ResourceServer().jwt();
 
         return http.build();
     }
